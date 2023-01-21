e339bf2a67bf12b33c3b31ba0ac5dbf2106f0055
jeongjihun <wlgns2234@naver.com>
2023-01-22 02:58:08 +0900

    refactor: 메뉴생성 구현코드 교체

--- src/main/java/menu/domain/Recommendation.java
+++ src/main/java/menu/domain/Recommendation.java
@@ -15,20 +15,10 @@ public class Recommendation {
     }
 
     public void creatRecommendaionMenu(HashMap<String, List<Food>> notEating) {
-        // notEating에 있는 코치수만큼 반복
+        // 각 코치에 대해 추천메뉴 생성
         for (Map.Entry<String, List<Food>> entry : notEating.entrySet()) {
-            RecommendationMenu menu = new RecommendationMenu(entry.getKey()); // 코치 추천메뉴서 생성(비었음)
-
-            //Algorithm.createRecommendationMenu(entry.getKey(), entry.getValue(), menu)
-            // entry.getValue();
-
-            // 월화수목금 5번에 걸쳐서 menu에 추천메뉴 삽입
-            menu.add(new Food("짜장면", "중식"));
-            menu.add(new Food("짬뽕", "중식"));
-            menu.add(new Food("군만두", "중식"));
-            menu.add(new Food("팔보채", "중식"));
-            menu.add(new Food("김밥", "중식"));
-
+            RecommendationMenu menu = new RecommendationMenu(entry.getKey());
+            Algorithm.createRecommendationMenu(entry.getKey(), entry.getValue(), menu);
             recommendation.add(menu);
         }
     }
